7e018ea8fccec5a6c7002c4ffe601d422e2ec318
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-07-20 20:39:02 +0200

    Implemented uptodate check for the template processor.

--- src/main/Java/wx-cicd-build-tools/src/main/java/com/github/jochenw/wx/cicd/build/ant/TemplateTask.java
+++ src/main/Java/wx-cicd-build-tools/src/main/java/com/github/jochenw/wx/cicd/build/ant/TemplateTask.java
@@ -1,5 +1,6 @@
 package com.github.jochenw.wx.cicd.build.ant;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,6 +16,8 @@ import java.nio.file.attribute.FileTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.FileScanner;
@@ -22,14 +25,41 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.FileSet;
 
+import com.github.jochenw.afw.core.util.Objects;
 import com.github.jochenw.afw.di.api.ComponentFactoryBuilder;
+import com.github.jochenw.afw.di.api.IComponentFactory;
 import com.github.jochenw.wx.cicd.build.template.SimpleTemplateCompiler;
 import com.github.jochenw.wx.cicd.build.template.Template;
 import com.github.jochenw.wx.cicd.build.template.Template.Model;
 import com.github.jochenw.wx.cicd.build.template.TemplateCompiler;
 import com.github.jochenw.wx.cicd.build.template.TemplateParseException;
+import com.github.jochenw.wx.cicd.build.template.TemplateUtils;
 
 public class TemplateTask extends Task {
+	public static class PropertyFile {
+		private Path file;
+
+		public void addText(String pText) {
+			setFile(pText);
+		}
+
+		public Path getFile() {
+			return file;
+		}
+		public void setFile(String pFile) {
+			if (pFile == null) {
+				throw new BuildException("The file attribute of a propertyFile element must not be null.");
+			}
+			if (pFile.length() == 0) {
+				throw new BuildException("The file attribute of a propertyFile element must not be empty.");
+			}
+			final Path p = Paths.get(pFile);
+			if (!Files.isRegularFile(p)) {
+				throw new BuildException("The file attribute of a propertyFile element must refer to an existing file:" + p);
+			}
+			file = p;
+		}
+	}
 	public static class TemplateSet extends FileSet {
 		private Charset charset;
 		private String prefix;
@@ -63,32 +93,39 @@ public class TemplateTask extends Task {
 		}
 	}
 
-	private final List<Path> propertyFiles = new ArrayList<>();
+
+	private final List<PropertyFile> propertyFiles = new ArrayList<>();
 	private final List<TemplateSet> templateSets = new ArrayList<>();
 	private Path outputDir;
+	private String startToken, endToken;
 	private boolean forced;
 
-	public List<Path> getPropertyFiles() {
+	public String getStartToken() { return Objects.notNull(startToken, "#{"); }
+	public void setStartToken(String pStartToken) { startToken = pStartToken; }
+	public String getEndToken() { return Objects.notNull(endToken, "}"); }
+
+	public void setEndToken(String endToken) {
+		this.endToken = endToken;
+	}
+
+	public List<PropertyFile> getPropertyFiles() {
 		return propertyFiles;
 	}
 
-	public void addPropertyFile(String pPropertyFile) {
-		if (pPropertyFile == null  ||  pPropertyFile.length() == 0) {
-			throw new BuildException("A property file must not be null, or empty.");
-		}
-		final Path path = Paths.get(pPropertyFile);
-		if (!Files.isRegularFile(path)) {
-			throw new BuildException("Invalid property file: Expected existing file, got " + pPropertyFile);
-		}
-		propertyFiles.add(path);
+	public PropertyFile createPropertyFile() {
+		final PropertyFile pf = new PropertyFile();
+		propertyFiles.add(pf);
+		return pf;
 	}
 
 	public List<TemplateSet> getTemplateSets() {
 		return templateSets;
 	}
 
-	public void addTemplateSet(TemplateSet pTemplateSet) {
-		templateSets.add(pTemplateSet);
+	public TemplateSet createTemplateSet() {
+		final TemplateSet ts = new TemplateSet();
+		templateSets.add(ts);
+		return ts;
 	}
 
 	public Path getOutputDir() {
@@ -115,25 +152,30 @@ public class TemplateTask extends Task {
 		if (templateSets.isEmpty()) {
 			throw new BuildException("No templates (nested element templateSet) given.");
 		}
-		final Properties props = getProperties();
+		final List<FileTime> propertyFileTimes = new ArrayList<FileTime>();
+		final Properties props = getProperties(propertyFileTimes::add);
 		final Model model = Model.of(props);
-		final TemplateCompiler tc = new ComponentFactoryBuilder()
+		final IComponentFactory cf = new ComponentFactoryBuilder()
 				.module(SimpleTemplateCompiler.MODULE)
-				.build()
-				.requireInstance(TemplateCompiler.class);
+				.build();
 		templateSets.forEach((ts) -> {
-			processTemplateSet(tc, ts, outDir, model);
+			processTemplateSet(cf, propertyFileTimes, ts, outDir, model);
 		});
 	}
 
-	protected Properties getProperties() {
+	protected Properties getProperties(Consumer<FileTime> pTimeConsumer) {
 		final Properties props = new Properties();
 		props.putAll(System.getProperties());
-		getPropertyFiles().forEach((p) -> {
+		getPropertyFiles().forEach((pf) -> {
+			final Path p = pf.getFile();
+			if (p == null) {
+				throw new BuildException("A nested propertyFile element must have its file attribute set.");
+			}
 			try (InputStream in = Files.newInputStream(p)) {
 				final Properties pr = new Properties();
 				pr.load(in);
 				props.putAll(pr);
+				pTimeConsumer.accept(Files.getLastModifiedTime(p));
 			} catch (IOException e) {
 				throw new BuildException("Failed to read property file " + p + ": " + e.getMessage(), e);
 			}
@@ -141,7 +183,8 @@ public class TemplateTask extends Task {
 		return props;
 	}
 
-	protected void processTemplateSet(TemplateCompiler pTemplateCompiler, TemplateSet pTemplateSet, Path pOutDir, Model pModel) {
+	protected void processTemplateSet(IComponentFactory pComponentFactory, List<FileTime> pPropertyFileTimes,
+			                          TemplateSet pTemplateSet, Path pOutDir, Model pModel) {
 		final Path baseDir = pTemplateSet.getDir(getProject()).toPath();
 		final Path targetDir;
 		final String templatePrefix = pTemplateSet.getPrefix();
@@ -155,25 +198,21 @@ public class TemplateTask extends Task {
 		fs.scan();
 		final String[] templateFiles = fs.getIncludedFiles();
 		getProject().log("Found " + templateFiles.length + " template files in directory " + baseDir);
+		final TemplateUtils templateUtils = pComponentFactory.requireInstance(TemplateUtils.class);
 		for (String templateFile : templateFiles) {
 			final Path sourceFile = baseDir.resolve(templateFile);
+			final SimpleTemplateCompiler stc = new SimpleTemplateCompiler(sourceFile.toString(), templateUtils, getStartToken(), getEndToken());
 			try {
 				final Path targetFile = targetDir.resolve(templateFile);
-				final FileTime targetTime;
-				if (!Files.isRegularFile(targetFile)) {
-					targetTime = null;
-				} else {
-					targetTime = Files.getLastModifiedTime(targetFile); 
-				}
-				final FileTime sourceTime = Files.getLastModifiedTime(sourceFile);
-				if (targetTime == null  ||  targetTime.compareTo(sourceTime) <= 0) {
-					getProject().log("Reading template file: " + sourceFile, Project.MSG_DEBUG);
-				} else {
+				final boolean uptodate = isUptodate(pPropertyFileTimes, sourceFile, targetFile);
+				if (uptodate) {
 					getProject().log("Ignoring template file, because it appears to be uptodate: " + sourceFile, Project.MSG_VERBOSE);
+				} else {
+					getProject().log("Reading template file: " + sourceFile, Project.MSG_DEBUG);
 					try (InputStream in = Files.newInputStream(sourceFile)) {
 						final Template template;
 						try {
-							template = pTemplateCompiler.compile(in, pTemplateSet.getCharset());
+							template = stc.compile(in, pTemplateSet.getCharset());
 						} catch (TemplateParseException tpe) {
 							throw new BuildException("Template file " + sourceFile + " was found to be invalid: " + tpe.getMessage(), tpe);
 						}
@@ -192,6 +231,36 @@ public class TemplateTask extends Task {
 				throw new BuildException("I/O error while processing template file " + sourceFile + ": " + e.getMessage(), e);
 			}
 		}
-		
+	}
+
+	protected boolean isUptodate(List<FileTime> pPropertyFileTimes, Path pSourceFile, Path pTargetFile) {
+		final Function<Path,FileTime> timeProvider = (p) -> {
+			try {
+				return Files.getLastModifiedTime(p);
+			} catch (FileNotFoundException e) {
+				return null;
+			} catch (IOException e) {
+				throw new BuildException("Unable to get file time for " + p + ": " + e.getMessage(), e);
+			}
+		};
+		final FileTime sourceTime = timeProvider.apply(pSourceFile);
+		final FileTime targetTime = timeProvider.apply(pTargetFile);
+		final boolean uptodate = isUptodate(sourceTime, targetTime);
+		if (!uptodate) {
+			return false;
+		}
+		for (FileTime ft : pPropertyFileTimes) {
+			if (!isUptodate(ft, targetTime)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	protected boolean isUptodate(FileTime pSourceTime, FileTime pTargetTime) {
+		if (pTargetTime == null) {
+			return false;
+		}
+		return pSourceTime.compareTo(pTargetTime) <= 0;
 	}
 }
